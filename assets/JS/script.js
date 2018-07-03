f6ca9fb6bc465ab185ea3a41013c85cd23a783d9
dembasy <dembasy1227@gmail.com>
2018-07-03 12:26:23 +0200

    correction du fichier html

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,1 @@
-$(function(){
-$("#mobile_number").validate({
-  rules: {
-    number: {required: true,number:true}
-  },
-  messages: {
-  number: {required: "Please Enter Your Mobile Number",number:"Please enter numbers Only"}
-  }
- });
-});
+
